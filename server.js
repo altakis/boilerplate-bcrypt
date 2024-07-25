ad4710a81e426682b9d3036e366359719b72e4d6
altakis <42413498+altakis@users.noreply.github.com>
2024-07-24 21:55:54 -0500

    End of coursework

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+const bcrypt = require('bcrypt');
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
